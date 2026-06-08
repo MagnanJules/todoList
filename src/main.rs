6f1b0bec9dd24c4f84da6ff06414bb772dcbc310
Jules Magnan <jules.magnan@epita.fr>
2026-06-08 21:56:34 +0200

    feat: showing decription

--- src/main.rs
+++ src/main.rs
@@ -64,14 +64,14 @@ impl App {
                     self.input.description.pop();
                 }
             }
-            KeyCode::Esc => {
-                self.screen = Screen::List;
-            }
             KeyCode::Enter => {
                 self.todos.push(Todo {
                     title: std::mem::take(&mut self.input.title),
                     description: std::mem::take(&mut self.input.description),
                 });
+            }
+            KeyCode::Esc => {
+                self.screen = Screen::List;
             }
             _ => {}
         }
@@ -132,13 +132,26 @@ fn render(frame: &mut Frame, app_state: &mut App) {
 }
 
 fn render_list(frame: &mut Frame, app_state: &mut App) {
+    let chunks = Layout::horizontal([Constraint::Percentage(40), Constraint::Percentage(60)])
+        .split(frame.area());
+
     let items: Vec<ListItem> = app_state
         .todos
         .iter()
         .map(|t| ListItem::new(t.title.clone()))
         .collect();
-    let list = List::new(items).block(Block::default().borders(Borders::all()).title("TODO"));
-    frame.render_widget(list, frame.area());
+    let list = List::new(items).block(Block::bordered().title("TODO"));
+    frame.render_widget(list, chunks[0]);
+
+    let description = app_state
+        .todos
+        .get(app_state.selected)
+        .map(|t| t.description.as_str())
+        .unwrap_or("No todo");
+
+    let desc_block = Paragraph::new(description).block(Block::bordered().title("Description"));
+
+    frame.render_widget(desc_block, chunks[1]);
 }
 fn render_create_todo(frame: &mut Frame, app_state: &mut App) {
     let popup_bloc = Block::bordered().title("Create TODO");
